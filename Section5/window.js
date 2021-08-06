1c7a1e4a8bfe813fe323186d050ca87218d0db2d
Nathan <nathanraysheryak@gmail.com>
2021-08-06 07:34:26 -0400

    colts solution

--- Section5/window.js
+++ Section5/window.js
@@ -2,7 +2,7 @@
 // Write a function called maxSubArraySum which accepts an arra of ints and a number (n). //
 // The function should calculate the max sum of n consecutive elements in the array.      //
 
-function maxSubArraySum(arr, n) {
+function my_maxSubArraySum(arr, n) {
   let largestSumSoFar = null;
 
   for (let i = 0; i < arr.length; i++) {
@@ -20,10 +20,31 @@ function maxSubArraySum(arr, n) {
 }
 
 // Notes
-//
+// Coles is better, i don't think it has a lot of readability though
 
 // Colts refactor
-function colt_window(arr, n) {}
+function maxSubArraySum(arr, n) {
+  let maxSum = 0;
+  let tempSum = 0;
+
+  // sets first window
+  for (let i = 0; i < n; i++) {
+    maxSum += arr[i];
+  }
+
+  // only one loop has occured so max = temp
+  tempSum = maxSum;
+
+  // (i = n) accounts for our offset from the first loop
+  for (let i = n; i < arr.length; i++) {
+    // tempSum, minus the first val, add the next val
+    tempSum = tempSum - arr[i - n] + arr[i];
+    // between the two identifies the largest sum
+    maxSum = Math.max(maxSum, tempSum);
+  }
+
+  return maxSum;
+}
 
 maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 2); // 10
 maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 4); // 17
