ef9034d053443157cb5f50a5a63f2a550290e5f5
Nathan <nathanraysheryak@gmail.com>
2021-08-07 15:16:40 -0400

    divide and conqure

---
+++ Section5/divideAndConquer.js
@@ -0,0 +1,6 @@
+// Divide a data set and chunk out what to do with it
+// With a sorted Arr for example
+// we want to find the index of a number in an array
+// we can divide the Arr in half
+// if the value we want is less than the last number of the first arr
+// then we discard the second half of the array and continue
