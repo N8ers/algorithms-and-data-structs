bb969df633250c4de190c75f5a1bbf2b8ec7ae05
Nathan <nathanraysheryak@gmail.com>
2021-08-11 07:42:58 -0400

    colts refactor ex4

--- Section6/ex4.js
+++ Section6/ex4.js
@@ -3,7 +3,7 @@
 // and checks wether there are any duplicates among the arguments
 // this can be done with `frequencyCounter` or `muleiplePointers` patterns
 
-function areThereDuplicates() {
+function my_areThereDuplicates() {
   let values = [...arguments];
 
   let valueFrequency = {};
@@ -19,6 +19,25 @@ function areThereDuplicates() {
     }
   }
 
+  console.log("FALSE");
+  return false;
+}
+
+// Notes
+// agin, its basically the same implamentation
+
+// Colts refactor
+function areThereDuplicates() {
+  let collection = {};
+  for (let val in arguments) {
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1;
+  }
+  for (let key in collection) {
+    if (collection[key] > 1) {
+      console.log("TRUE");
+      return true;
+    }
+  }
   console.log("FALSE");
   return false;
 }
